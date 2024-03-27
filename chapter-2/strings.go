a0fad12f20891561a99d9eb5993359fc06f0a201
Mohamed Zahi <mohamedzahi33@gmail.com>
2024-03-28 03:55:12 +0400

    update 104

---
+++ chapter-2/strings.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() { //rename it
+	// myStr := "Hello world"
+	// for i := 0; i < len(myStr); i++ {
+	// 	char := myStr[i]
+	// 	fmt.Printf("%c\n", char)
+	// }
+	myStr := "hello"
+	fmt.Printf("%s\n", myStr)
+}
