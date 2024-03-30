a1e31ef563c15accb0dff2147cbca6522810753d
Mohamed Zahi <mohamedzahi33@gmail.com>
2024-03-31 03:07:54 +0400

    go done

--- chapter-3/deg_converter.go
+++ chapter-3/deg_converter.go
@@ -1,25 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	var convType byte
+	//var convType string
 
 	fmt.Println("Please Choose:\n a: for F to C\n b: fot C to F")
-	//fmt.Scanf("%c", &convType)
-	convType = 'a'
+	//fmt.Scanf("%s", &convType)
+	reader := bufio.NewReader(os.Stdin)
+	convType, _, err := reader.ReadRune()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//convType = 'a'
 	if convType == 'b' { //C to F
 		var c float32
 		fmt.Println("Enter the degree in C:")
 		fmt.Scanf("%f", &c)
-		f := c*(9/5) + 32
+		f := c*(9.0/5.0) + 32
 		fmt.Printf("The degree in F is %f\n", f)
 	} else if convType == 'a' { //F to C
 		var f float32
 		fmt.Println("Enter the degree in F:")
 		fmt.Scanf("%f", &f)
-		c := (f - 32) * (5 / 9)
+		c := (f - 32) * (5.0 / 9.0)
 		fmt.Printf("The degree in C is %f\n", c)
 	} else {
 		fmt.Println("bad choice!")
